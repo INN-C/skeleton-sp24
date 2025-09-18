54222a800ee1c047ae4852a5ef39dbcfbb7ccc50
INN-C <inn.c@qq.com>
2025-09-18 09:44:27 +0800

    lab01: Fixed sum method in Arithmetic.java

--- lab01/src/Arithmetic.java
+++ lab01/src/Arithmetic.java
@@ -5,6 +5,8 @@ import edu.princeton.cs.algs4.StdIn;
  * */
 public class Arithmetic {
 
+    // just try it
+
     /** Computes product of two ints.
      * @param a Value 1
      * @param b Value 2
@@ -32,4 +34,5 @@ public class Arithmetic {
         System.out.println("The product of " + num1 + " and " + num2 + " is: " + product(num1, num2));
         System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum(num1, num2));
     }
+
 }
